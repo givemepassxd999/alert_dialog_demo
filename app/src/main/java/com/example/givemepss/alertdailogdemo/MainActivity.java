f01cf426f5f73753809d228ea398940c39a94430
Rick <givemepassxd999@gmail.com>
2017-09-05 19:30:37 +0800

    修正AlertDialog範例

--- app/src/main/java/com/example/givemepss/alertdailogdemo/MainActivity.java
+++ app/src/main/java/com/example/givemepss/alertdailogdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -15,31 +16,45 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button alertDialog1;
-    private Button alertDialog2;
-    private Button alertDialog3;
+    private Button normalDialog;
+    private Button listDialog;
+    private Button singleDialog;
+    private Button multiDialog;
+    private Button customDialog;
     private List<String> lunch;
     private View.OnClickListener buttonListener = new View.OnClickListener(){
         @Override
         public void onClick(View view) {
             switch(view.getId()){
-                case R.id.alertdialog1:
-                    setAlertDialog1Event();
+                case R.id.normal_dialog:
+                    normalDialogEvent();
                     break;
-                case R.id.alertdialog2:
-                    setAlertDialog2Event();
+                case R.id.list_dialog:
+                    listDialogEvent();
                     break;
-                case R.id.alertdialog3:
-                    setAlertDialog3Event();
+                case R.id.single_choice:
+                    singleDialogEvent();
+                    break;
+                case R.id.multi_check:
+                    multiDialogEvent();
+                    break;
+                case R.id.custom_dialog:
+                    customDialogEvent();
                     break;
             }
         }
     };
-
+    private int singleChoiceIndex;
+    private int checkedIndex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+        initView();
+    }
+
+    private void initData() {
         lunch = new ArrayList<>();
         lunch.add(getString(R.string.lunch1));
         lunch.add(getString(R.string.lunch2));
@@ -47,23 +62,26 @@ public class MainActivity extends AppCompatActivity {
         lunch.add(getString(R.string.lunch4));
         lunch.add(getString(R.string.lunch5));
         lunch.add(getString(R.string.lunch6));
-        getButtonView();
+    }
+
+    public void initView(){
+        normalDialog = (Button) findViewById(R.id.normal_dialog);
+        listDialog = (Button) findViewById(R.id.list_dialog);
+        singleDialog = (Button) findViewById(R.id.single_choice);
+        multiDialog = (Button) findViewById(R.id.multi_check);
+        customDialog = (Button) findViewById(R.id.custom_dialog);
         setButtonEvent();
     }
 
-    public void getButtonView(){
-        alertDialog1 = (Button)findViewById(R.id.alertdialog1);
-        alertDialog2 = (Button)findViewById(R.id.alertdialog2);
-        alertDialog3 = (Button)findViewById(R.id.alertdialog3);
-    }
-
     public void setButtonEvent(){
-        alertDialog1.setOnClickListener(buttonListener);
-        alertDialog2.setOnClickListener(buttonListener);
-        alertDialog3.setOnClickListener(buttonListener);
+        normalDialog.setOnClickListener(buttonListener);
+        listDialog.setOnClickListener(buttonListener);
+        singleDialog.setOnClickListener(buttonListener);
+        multiDialog.setOnClickListener(buttonListener);
+        customDialog.setOnClickListener(buttonListener);
     }
 
-    private void setAlertDialog1Event(){
+    private void normalDialogEvent(){
         new AlertDialog.Builder(MainActivity.this)
             .setTitle(R.string.lunch_time)
             .setMessage(R.string.want_to_eat)
@@ -88,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
             .show();
     }
 
-    private void setAlertDialog3Event() {
+    private void customDialogEvent() {
         final View item = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_layout, null);
         new AlertDialog.Builder(MainActivity.this)
             .setTitle(R.string.input_ur_name)
@@ -96,20 +114,84 @@ public class MainActivity extends AppCompatActivity {
             .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    EditText editText = (EditText) item.findViewById(R.id.edittext);
-                    Toast.makeText(getApplicationContext(), getString(R.string.hi) + editText.getText().toString(), Toast.LENGTH_SHORT).show();
+                    EditText editText = (EditText) item.findViewById(R.id.edit_text);
+                    String name = editText.getText().toString();
+                    if(TextUtils.isEmpty(name)){
+                        Toast.makeText(getApplicationContext(), R.string.input_ur_name, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), getString(R.string.hi) + name, Toast.LENGTH_SHORT).show();
+                    }
                 }
             })
             .show();
     }
 
-    private void setAlertDialog2Event() {
+    private void listDialogEvent() {
         new AlertDialog.Builder(MainActivity.this)
             .setItems(lunch.toArray(new String[lunch.size()]), new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     String name = lunch.get(which);
                     Toast.makeText(getApplicationContext(), getString(R.string.u_eat) + name, Toast.LENGTH_SHORT).show();
+                }
+            })
+            .show();
+    }
+
+    private void singleDialogEvent(){
+        new AlertDialog.Builder(MainActivity.this)
+            .setSingleChoiceItems(lunch.toArray(new String[lunch.size()]), singleChoiceIndex,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        singleChoiceIndex = which;
+                    }
+                })
+            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, "你選擇的是"+lunch.get(singleChoiceIndex), Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+            })
+            .show();
+    }
+
+    private void multiDialogEvent(){
+        final List<Boolean> checkedStatusList = new ArrayList<>();
+        for(String s : lunch){
+            checkedStatusList.add(false);
+        }
+        new AlertDialog.Builder(MainActivity.this)
+            .setMultiChoiceItems(lunch.toArray(new String[lunch.size()]), new boolean[lunch.size()],
+                new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        checkedStatusList.set(which, isChecked);
+                    }
+                })
+            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    StringBuilder sb = new StringBuilder();
+                    boolean isEmpty = true;
+                    for(int i = 0; i < checkedStatusList.size(); i++){
+                        if(checkedStatusList.get(i)){
+                            sb.append(lunch.get(i));
+                            sb.append(" ");
+                            isEmpty = false;
+                        }
+                    }
+                    if(!isEmpty){
+                        Toast.makeText(MainActivity.this, "你選擇的是"+sb.toString(), Toast.LENGTH_SHORT).show();
+                        for(String s : lunch){
+                            checkedStatusList.add(false);
+                        }
+                    } else{
+                        Toast.makeText(MainActivity.this, "請勾選項目", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
             })
             .show();
